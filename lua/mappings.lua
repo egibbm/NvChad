5a3eed6eeb3cb66a698430e098338db479a90b58
Agastiya S. Mohammad <39547572+egibbm@users.noreply.github.com>
2021-06-20 10:40:07 +0700

    Added woi's key_mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -34,6 +34,97 @@ map("v", "x", [=[ "_x ]=], opt)
 -- map("n", "<C-s>", [[ <Cmd> w <CR>]], opt)
 -- vim.cmd("inoremap jh <Esc>")
 
+-- easy wrap toggling
+map("n", "<Leader>w", ":set wrap!<cr>", opt)
+map("n", "<Leader>W", ":set nowrap<cr>", opt)
+
+-- close all other windows (in the current tab)
+map("n", "gW", ":only<cr>", opt)
+
+-- shortcuts for frequenly used files
+map("n", "gs", ":e db/schema.rb<cr>", opt)
+map("n", "gr", ":e config/routes.rb<cr>", opt)
+map("n", "gm", ":e Gemfile<cr>", opt)
+
 -- Commenter Keybinding
 map("n", "<C-_>", ":CommentToggle<CR>", {noremap = true, silent = true})
 map("v", "<C-_>", ":CommentToggle<CR>", {noremap = true, silent = true})
+
+-- insert blank lines without going into insert mode
+map("n", "go", "o<esc>", opt)
+map("n", "gO", "O<esc>", opt)
+
+-- mapping the jumping between splits. Hold control while using vim nav.
+map("n", "<C-J>", "<C-W>j", opt)
+map("n", "<C-K>", "<C-W>k", opt)
+map("n", "<C-H>", "<C-W>h", opt)
+map("n", "<C-L>", "<C-W>l", opt)
+
+-- Yank from the cursor to the end of the line, to be consistent with C and D.
+map("n", "Y", "y$", opt)
+
+-- select the lines which were just pasted
+map("n", "vv", "`[V`]", opt)
+
+-- clean up trailing whitespace
+map("", "<Leader>c", ":StripTrailingWhitespaces<cr>", opt)
+
+-- compress excess whitespace on current line
+map("", "<Leader>e", ":s/\\v(\\S+)\\s+/\\1 /<cr>:nohl<cr>", opt)
+
+-- delete all buffers
+map("", "<Leader>d", ":bufdo bd<cr>", opt)
+
+-- map spacebar to clear search highlight
+map("n", "<Leader><space>", ":noh<cr>", opt)
+
+-- buffer resizing mappings (shift + arrow key)
+map("n", "<S-Up>", "<c-w>+", opt)
+map("n", "<S-Down>", "<c-w>-", opt)
+map("n", "<S-Left>", "<c-w><<c-w><<c-w><", opt)
+map("n", "<S-Right>", "<c-w>><c-w>><c-w>>", opt)
+
+-- reindent the entire file
+map("n", "<Leader>I", "gg=G``<cr>", opt)
+
+-- insert the path of currently edited file into a command
+-- Command mode: Ctrl-P
+map("c", "<C-S-P>", '<C-R>=expand("%:p:h") . "/" <cr>', opt)
+
+
+
+-- Tricks
+map("", "n", "nzz", opt)
+map("", "N", "Nzz", opt)
+map("", "<C-o>", "<C-o>zz", opt)
+map("", "<C-i>", "<C-i>zz", opt)
+
+-- PHP
+map("i", "<C-l>", "->", opt)
+map("i", "<C-k>", "=>", opt)
+
+-- CTRL-X and SHIFT-Del are Cut
+map("v", "<C-X>", '"+x', opt)
+map("v", "<S-Del>", '"+x', opt)
+
+-- CTRL-C and CTRL-Insert are Copy
+map("v", "<C-C>", '"+y', opt)
+map("v", "<C-Insert>", '"+y', opt)
+
+-- CTRL-V and SHIFT-Insert are Paste
+map("",  "<S-Insert>", '"+gP', opt)
+map("i", "<S-Insert>", '<Esc>"+gP', opt)
+map("c", "<S-Insert>", "<C-R>+", opt)
+
+
+-- Navigation for tab
+map("", "<Leader>1", "1gt", opt)
+map("", "<Leader>2", "2gt", opt)
+map("", "<Leader>3", "3gt", opt)
+map("", "<Leader>4", "4gt", opt)
+map("", "<Leader>5", "5gt", opt)
+map("", "<Leader>6", "6gt", opt)
+map("", "<Leader>7", "7gt", opt)
+map("", "<Leader>8", "8gt", opt)
+map("", "<Leader>9", "9gt", opt)
+map("", "<Leader>0", ":tablast<cr>", opt)
