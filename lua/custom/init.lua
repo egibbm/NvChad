96177e0c7cdedb571fe0ecfc6d838630d278fc34
Agastiya S. Mohammad <39547572+egibbm@users.noreply.github.com>
2022-01-12 22:54:20 +0700

    fixed: change of struct in custom.init

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,27 +1,9 @@
 -- This is where you custom modules and plugins goes.
 -- See the wiki for a guide on how to extend NvChad
 
-local hooks = require "core.hooks"
+-- MAPPINGS
+local map = require("core.utils").map
 
--- NOTE: To use this, make a copy with `cp example_init.lua init.lua`
-
---------------------------------------------------------------------
-
--- To modify packaged plugin configs, use the overrides functionality
--- if the override does not exist in the plugin config, make or request a PR,
--- or you can override the whole plugin config with 'chadrc' -> M.plugins.default_plugin_config_replace{}
--- this will run your config instead of the NvChad config for the given plugin
-
--- hooks.override("lsp", "publish_diagnostics", function(current)
---   current.virtual_text = false;
---   return current;
--- end)
-
--- To add new mappings, use the "setup_mappings" hook,
--- you can set one or many mappings
--- example below:
-
-hooks.add("setup_mappings", function(map)
    -- easy wrap toggling
    map("n", "<Leader>w", ":set wrap!<cr>")
    map("n", "<Leader>W", ":set nowrap<cr>")
@@ -102,14 +84,11 @@ hooks.add("setup_mappings", function(map)
    map("", "<Leader>8", "8gt")
    map("", "<Leader>9", "9gt")
    map("", "<Leader>0", ":tablast<cr>")
-end)
 
--- To add new plugins, use the "install_plugin" hook,
--- NOTE: we heavily suggest using Packer's lazy loading (with the 'event' field)
--- see: https://github.com/wbthomason/packer.nvim
--- examples below:
+-- Install plugins
+local customPlugins = require "core.customPlugins"
 
-hooks.add("install_plugins", function(use)
+customPlugins.add(function(use)
    use {
       "jose-elias-alvarez/null-ls.nvim",
       after = "nvim-lspconfig",
@@ -195,8 +174,9 @@ hooks.add("install_plugins", function(use)
    }
 end)
 
--- alternatively, put this in a sub-folder like "lua/custom/plugins/mkdir"
--- then source it with
+-- NOTE: we heavily suggest using Packer's lazy loading (with the 'event','cmd' fields)
+-- see: https://github.com/wbthomason/packer.nvim
+-- https://nvchad.github.io/config/walkthrough
 
 -- require "custom.plugins.mkdir"
 require "custom.autocmds"
